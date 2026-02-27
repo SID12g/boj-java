b04fc8eede875e5b1e4e1f984ca7276b0198c586
sid12g <sead12g@gmail.com>
2026-02-27 15:57:31 +0900

    add 10871

--- src/bronze/b5_10871.java
+++ src/bronze/b5_10871.java
@@ -1,4 +1,18 @@
 package bronze;
 
+import java.util.Scanner;
+
 public class b5_10871 {
+    static void main() {
+        int n, x, k;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        x = scanner.nextInt();
+        for(int i = 0; i<n; i++) {
+            k = scanner.nextInt();
+            if(k < x) {
+                System.out.println(k);
+            }
+        }
+    }
 }
