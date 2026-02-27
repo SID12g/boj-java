12f133ce7cc2d499bb590f710b367e6d0dce96dc
sid12g <sead12g@gmail.com>
2026-02-27 16:05:06 +0900

    add 10869

---
+++ src/bronze/b5_10869.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class b5_10869 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
