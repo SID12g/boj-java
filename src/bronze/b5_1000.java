7e9d716d7ab5e7594c6a00161272c2ec08792b19
sid12g <sead12g@gmail.com>
2026-02-27 16:01:08 +0900

    add 1000

---
+++ src/bronze/b5_1000.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class b5_1000 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
